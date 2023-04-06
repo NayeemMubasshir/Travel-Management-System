0e57a7569e46d39d202126b4433fc76de524bedf
Nayeem Mubasshir <nayeemhaque19@gmail.com>
2023-04-06 18:55:10 +0600

    commit test

---
+++ DATABASE FILE/traveldatabasemain.sql
@@ -0,0 +1,29 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+--
+-- Database: traveldatabasemain
+--
+
+
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`user_id`, `username`, `password`, `name`) VALUES
+(2, 'admin', 'admin', 'admin');
+
