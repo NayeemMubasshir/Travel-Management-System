6825aca4343392d8bb15e3ea8e452ed00ad42ef0
NayeemMubasshir <nayeemhaque19@gmail.com>
2023-04-09 23:44:23 +0600

    database added

--- DATABASE FILE/traveldatabasemain.sql
+++ DATABASE FILE/traveldatabasemain.sql
@@ -9,9 +9,7 @@ SET time_zone = "+00:00";
 
 
 
---
--- Table structure for table `admin`
---
+
 
 CREATE TABLE `admin` (
   `user_id` int(11) NOT NULL,
@@ -20,10 +18,56 @@ CREATE TABLE `admin` (
   `name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `admin`
---
+
 
 INSERT INTO `admin` (`user_id`, `username`, `password`, `name`) VALUES
 (2, 'admin', 'admin', 'admin');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE TABLE `travelevents` (
+  `id` int(11) NOT NULL,
+  `message` varchar(100) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+INSERT INTO `travelevents` (`id`, `message`, `date`) VALUES
+(7, 'Event in sajek', '2023-03-30');
+
+
+
+
+
+
+
+CREATE TABLE `packages` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `charge` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+INSERT INTO `packages` (`id`, `name`, `charge`) VALUES
+(1, 'Long tour: 3 months', '300'),
+(2, 'Short tour: 2 weeks', '100'),
+(3, 'International tour', '1000');
+
 
