dbaa5bcfa6b7a04801901a3e2de6a337ddeb1f06
NayeemMubasshir <nayeemhaque19@gmail.com>
2023-04-15 23:54:14 +0600

    classification pages

--- customer/pages/index.php
+++ customer/pages/index.php
@@ -1,23 +1,8 @@
-<?php
-session_start();
-//the isset function to check username is already loged in and stored on the session
-if(!isset($_SESSION['user_id'])){
-header('location:../index.php');	
-}
-include "setconntotraveldb.php";
-$qry="SELECT services, count(*) as number FROM members GROUP BY services";
-$result=mysqli_query($con,$qry);
-$qry="SELECT gender, count(*) as enumber FROM members GROUP BY gender";
-$result3=mysqli_query($con,$qry);
-$qry="SELECT designation, count(*) as snumber FROM staffs GROUP BY designation";
-$result5=mysqli_query($con,$qry);
-?>
 
 <!DOCTYPE html>
-
 <html lang="en">
 <head>
-<title>Travel admin</title>
+<title>travel System</title>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <link rel="stylesheet" href="../css/bootstrap.min.css" />
@@ -25,68 +10,94 @@ $result5=mysqli_query($con,$qry);
 <link rel="stylesheet" href="../css/fullcalendar.css" />
 <link rel="stylesheet" href="../css/matrix-style.css" />
 <link rel="stylesheet" href="../css/matrix-media.css" />
-<link href="../font-awesome/css/all.css" rel="stylesheet" />
-<link href="../font-awesome/css/fontawesome.css" rel="stylesheet" />
+<link href="../font-awesome/css/font-awesome.css" rel="stylesheet" />
 <link rel="stylesheet" href="../css/jquery.gritter.css" />
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
-
-
-
-
-
-
-
-
-
-
-    
 </head>
 <body>
 
 <!--Header-part-->
 <div id="header">
-  <h1><a href="dashboard.html">Nayeem Travelling Co Admin</a></h1>
+  <h1><a href="index.php">Nayeem Travelling Co System</a></h1>
 </div>
 <!--close-Header-part--> 
 
 
 <!--top-Header-menu-->
-<?php include 'includes/topheader.php'?>
+<?php include '../includes/topheader.php'?>
 <!--close-top-Header-menu-->
 
-
+<!--start-top-serch-->
+<!-- <div id="search">
+  <input type="hidden" placeholder="Search here..."/>
+  <button type="submit" class="tip-bottom" title="Search"><i class="icon-search icon-white"></i></button>
+</div> -->
+<!--close-top-serch-->
 <!--sidebar-menu-->
-  <?php $page='dashboard'; include 'includes/sidebar.php'?>
+<?php $page="dashboard"; include '../includes/sidebar.php'?>
 <!--sidebar-menu-->
 
 <!--main-container-part-->
 <div id="content">
 <!--breadcrumbs-->
   <div id="content-header">
-    <div id="breadcrumb"> <a href="index.php" title="You're right here" class="tip-bottom"><i class="fa fa-home"></i> Home</a></div>
+    <div id="breadcrumb"> <a href="index.php" title="You're right here" class="tip-bottom"><i class="icon-home"></i> Home</a></div>
   </div>
 <!--End-breadcrumbs-->
 
 <!--Action boxes-->
-  
+  <div class="container-fluid">
+    
 <!--End-Action boxes-->    
 
-<!--Chart-box-->    
-  
-    <!-- End of row-fluid -->
-
-    
-
-    
-
-      
-	
-<!--End-Chart-box--> 
-    <!-- <hr/> -->
     <div class="row-fluid">
+	  
       <div class="span6">
+       
+       <div class="widget-box">
+          <div class="widget-title"> <span class="icon"><i class="icon-time"></i></span>
+            <h5>My To-Do List</h5>
+          </div>
+          <div class="widget-content nopadding">
+
+        <?php
+            include "setconntotraveldb.php";
+            include "session.php";
+            $qry="SELECT * FROM faqandsupp WHERE user_id='".$_SESSION['user_id']."'";
+            $result=mysqli_query($con,$qry);
+
+            echo"<table class='table table-striped table-bordered'>
+              <thead>
+                <tr>
+                  <th>Description</th>
+                  <th>Status</th>
+                  <th>Opts</th>
+                </tr>
+              </thead>";
+              while($row=mysqli_fetch_array($result)){
+              echo"<tbody>
+                <tr>
+                  <td class='taskDesc'><a href='to-do.php'><i class='icon-plus-sign'></i></a>".$row['task_desc']."</td>
+                  <td class='taskStatus'><span class='in-progress'>".$row['task_status']."</span></td>
+                  <td class='taskOptions'><a href='update-todo.php?id=".$row['id']."' class='tip-top' data-original-title='Update'><i class='icon-edit'></i></a>  <a href='actions/remove-todo.php?id=".$row['id']."' class='tip-top' data-original-title='Done'><i class='icon-ok'></i></a></td>
+                </tr>
+				
+               
+              </tbody>";
+              }
+        ?>
+
+            </table>
+          </div>
+        </div>
+       
+      
+       
+      </div> <!-- End of ToDo List Bar -->
+	  
+	  <div class="span6">
         <div class="widget-box">
-          <div class="widget-title bg_ly" data-toggle="collapse" href="#collapseG2"><span class="icon"><i class="fas fa-chevron-down"></i></span>
+          <div class="widget-title bg_ly" data-toggle="collapse" href="#collapseG2"><span class="icon"><i class="icon-chevron-down"></i></span>
             <h5>travel travelevent</h5>
           </div>
           <div class="widget-content nopadding collapse in" id="collapseG2">
@@ -96,8 +107,8 @@ $result5=mysqli_query($con,$qry);
               <?php
 
                 include "setconntotraveldb.php";
-                $qry="SELECT * FROM travelevents";
-                $result=mysqli_query($conn,$qry);
+                $qry="select * from travelevents";
+                  $result=mysqli_query($con,$qry);
                   
                 while($row=mysqli_fetch_array($result)){
                   echo"<div class='user-thumb'> <img width='70' height='40' alt='User' src='../img/demo/av1.jpg'> </div>";
@@ -110,71 +121,20 @@ $result5=mysqli_query($con,$qry);
                 echo"</div>";
                 echo"</li>";
               ?>
-
-              <a href="manage-travelevent.php"><button class="btn btn-warning btn-mini">View All</button></a>
+                <a href="travelevent.php"><button class="btn btn-warning btn-mini">View All</button></a>
               </li>
             </ul>
           </div>
         </div>
-       
-         
-      </div>
-      <div class="span6">
-       
-      <div class="widget-box">
-          <div class="widget-title"> <span class="icon"><i class="fas fa-tasks"></i></span>
-            <h5>Travel FAQ</h5>
-          </div>
-          <div class="widget-content">
-            <div class="faqandsupp">
-              <ul>
-              <?php
-
-                include "setconntotraveldb.php";
-                $qry="SELECT * FROM faqandsupp";
-                $result=mysqli_query($con,$qry);
-
-                while($row=mysqli_fetch_array($result)){ ?>
-
-                <li class='clearfix'> 
-                                                                        
-                    <div class='txt'> <?php echo $row["task_desc"]?> <?php if ($row["task_status"] == "Pending") { echo '<span class="by label label-info">Pending</span>';} else { echo '<span class="by label label-success">In Progress</span>'; }?></div>
-                
-               <?php }
-                echo"</li>";
-              echo"</ul>";
-              ?>
-            </div>
-          </div>
-        </div>
-       
-                </div>
-       
-      </div> <!-- End of ToDo List Bar -->
-    </div><!-- End of travelevent Bar -->
+      </div> <!-- end of travelevent -->
+	  
+    </div><!-- End of row-fluid -->
   </div><!-- End of container-fluid -->
 </div><!-- End of content-ID -->
 
 <!--end-main-container-part-->
 
-<!--Footer-part-->
 
-
-
-<style>
-#footer {
-  color: white;
-}
-
-#piechart {
-  width: 800px; 
-  height: 280px;  
-  margin-left:auto; 
-  margin-right:auto;
-}
-</style>
-
-<!--end-Footer-part-->
 
 <script src="../js/excanvas.min.js"></script> 
 <script src="../js/jquery.min.js"></script> 
@@ -187,7 +147,7 @@ $result5=mysqli_query($con,$qry);
 <script src="../js/matrix.js"></script> 
 <script src="../js/matrix.dashboard.js"></script> 
 <script src="../js/jquery.gritter.min.js"></script> 
-
+<script src="../js/matrix.interface.js"></script> 
 <script src="../js/matrix.chat.js"></script> 
 <script src="../js/jquery.validate.js"></script> 
 <script src="../js/matrix.form_validation.js"></script> 
