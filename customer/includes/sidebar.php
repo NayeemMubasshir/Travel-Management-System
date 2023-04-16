8d9a83b98b4861886f68f0a1fb85618f11ea5cad
NayeemMubasshir <nayeemhaque19@gmail.com>
2023-04-16 17:29:20 +0600

    admin to customer

---
+++ customer/includes/sidebar.php
@@ -0,0 +1,28 @@
+<div id="sidebar"><a href="#" class="visible-phone"><i class="icon icon-home"></i> Dashboard</a>
+  <ul>
+
+  
+
+   
+    
+
+
+    <li class="submenu"> <a href="#"><i class="fas fa-users"></i> <span>Manage Members</span></a>
+      <ul>
+        <li class="<?php if($page=='members'){ echo 'active'; }?>"><a href="members.php"><i class="fas fa-arrow-right"></i> List All Members</a></li>
+        
+        
+        <li class="<?php if($page=='members-update'){ echo 'active'; }?>"><a href="edit-member.php"><i class="fas fa-arrow-right"></i> Update Member Details</a></li>
+      </ul>
+    </li>
+
+    
+
+    
+
+    
+
+
+
+  </ul>
+</div>
