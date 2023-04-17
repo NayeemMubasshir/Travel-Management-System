cbf8079dbc807f3d8fa485eae6f8e5109c31cb50
NayeemMubasshir <nayeemhaque19@gmail.com>
2023-04-17 10:50:34 +0600

    travelmanager mainpage

---
+++ staff/staff-pages/index.php
@@ -0,0 +1,184 @@
+<?php
+session_start();
+//the isset function to check username is already loged in and stored on the session
+if(!isset($_SESSION['user_id'])){
+header('location:../index.php');	
+}
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+<title>travel System Staff A/C</title>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="stylesheet" href="../css/bootstrap.min.css" />
+<link rel="stylesheet" href="../css/bootstrap-responsive.min.css" />
+<link rel="stylesheet" href="../css/fullcalendar.css" />
+<link rel="stylesheet" href="../css/matrix-style.css" />
+<link rel="stylesheet" href="../css/matrix-media.css" />
+<link href="../font-awesome/css/font-awesome.css" rel="stylesheet" />
+<link rel="stylesheet" href="../css/jquery.gritter.css" />
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+<!--Header-part-->
+<div id="header">
+  <h1><a href="dashboard.html">Nayeem Travelling Co</a></h1>
+</div>
+<!--close-Header-part--> 
+
+
+<!--top-Header-menu-->
+<?php $page="dashboard"; include '../includes/header.php'?>
+<!--close-top-Header-menu-->
+
+
+<!--sidebar-menu-->
+<?php $page="dashboard"; include '../includes/sidebar.php'?>
+<!--sidebar-menu-->
+
+<!--main-container-part-->
+<div id="content">
+<!--breadcrumbs-->
+  <div id="content-header">
+    <div id="breadcrumb"> <a href="index.php" title="You're right here" class="tip-bottom"><i class="icon-home"></i> Home</a></div>
+  </div>
+<!--End-breadcrumbs-->
+
+<!--Action boxes-->
+  <div class="container-fluid">
+
+<!--End-Action boxes-->    
+
+    
+    
+	
+<!--End-Chart-box--> 
+    <hr/>
+    <div class="row-fluid">
+      <div class="span6">
+        <div class="widget-box">
+          <div class="widget-title bg_ly" data-toggle="collapse" href="#collapseG2"><span class="icon"><i class="icon-chevron-down"></i></span>
+            <h5>Travel Events</h5>
+          </div>
+          <div class="widget-content nopadding collapse in" id="collapseG2">
+            <ul class="recent-posts">
+              <li>
+
+              <?php
+
+                include "setconntotraveldb.php";
+                $qry="select * from travelevents";
+                  $result=mysqli_query($conn,$qry);
+                  
+                while($row=mysqli_fetch_array($result)){
+                  echo"<div class='user-thumb'> <img width='70' height='40' alt='User' src='../img/demo/av1.jpg'> </div>";
+                  echo"<div class='article-post'>"; 
+                  echo"<span class='user-info'> By: System Administrator / Date: ".$row['date']." </span>";
+                  echo"<p><a href='#'>".$row['message']."</a> </p>";
+                 
+                }
+
+                echo"</div>";
+                echo"</li>";
+              ?>
+
+                <button class="btn btn-warning btn-mini">View All</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+       
+         
+      </div>
+      <div class="span6">
+       
+        <div class="widget-box">
+          <div class="widget-title"> <span class="icon"><i class="icon-tasks"></i></span>
+            <h5>Customer's FAQ and Supps</h5>
+          </div>
+          <div class="widget-content">
+            <div class="faqandsupp">
+              <ul>
+              <?php
+
+                include "setconntotraveldb.php";
+                $qry="SELECT * FROM faqandsupp";
+                $result=mysqli_query($conn,$qry);
+
+                while($row=mysqli_fetch_array($result)){ ?>
+
+                <li class='clearfix'> 
+                                                                        
+                    <div class='txt'> <?php echo $row["task_desc"]?> <?php if ($row["task_status"] == "Pending") { echo '<span class="date badge badge-important">Pending</span>';} else { echo '<span class="date badge badge-success">In Progress</span>'; }?></div>
+                
+               <?php }
+                echo"</li>";
+              echo"</ul>";
+              ?>
+            </div>
+          </div>
+        </div>
+       
+      
+       
+      </div> <!-- End of ToDo List Bar -->
+    </div><!-- End of travelevent Bar -->
+  </div><!-- End of container-fluid -->
+</div><!-- End of content-ID -->
+
+<!--end-main-container-part-->
+
+
+
+<script src="../js/excanvas.min.js"></script> 
+<script src="../js/jquery.min.js"></script> 
+<script src="../js/jquery.ui.custom.js"></script> 
+<script src="../js/bootstrap.min.js"></script> 
+<script src="../js/jquery.flot.min.js"></script> 
+<script src="../js/jquery.flot.resize.min.js"></script> 
+<script src="../js/jquery.peity.min.js"></script> 
+<script src="../js/fullcalendar.min.js"></script> 
+<script src="../js/matrix.js"></script> 
+<script src="../js/matrix.dashboard.js"></script> 
+<script src="../js/jquery.gritter.min.js"></script> 
+<!-- <script src="../js/matrix.interface.js"></script>  -->
+<script src="../js/matrix.chat.js"></script> 
+<script src="../js/jquery.validate.js"></script> 
+<script src="../js/matrix.form_validation.js"></script> 
+<script src="../js/jquery.wizard.js"></script> 
+<script src="../js/jquery.uniform.js"></script> 
+<script src="../js/select2.min.js"></script> 
+<script src="../js/matrix.popover.js"></script> 
+<script src="../js/jquery.dataTables.min.js"></script> 
+<script src="../js/matrix.tables.js"></script> 
+
+<script type="text/javascript">
+  // This function is called from the pop-up menus to transfer to
+  // a different page. Ignore if the value returned is a null string:
+  function goPage (newURL) {
+
+      // if url is empty, skip the menu dividers and reset the menu selection to default
+      if (newURL != "") {
+      
+          // if url is "-", it is this page -- reset the menu:
+          if (newURL == "-" ) {
+              resetMenu();            
+          } 
+          // else, send page to designated URL            
+          else {  
+            document.location.href = newURL;
+          }
+      }
+  }
+
+// resets the menu selection upon entry to this page:
+function resetMenu() {
+   document.gomenu.selector.selectedIndex = 2;
+}
+</script>
+</body>
+</html>
